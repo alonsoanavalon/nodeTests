c1668dc3a3494b2a46e2a0e094af18859306d48a
keyzen <alonso.anavalon@gmail.com>
2021-09-16 23:40:12 -0300

    nada

--- src/app.js
+++ src/app.js
@@ -29,9 +29,10 @@ const hbs = exphbs.create({
 const app = express()
 
 
-// Settings
+// Settingss
 
-app.set('port', process.env.PORT || 3000)
+
+
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
